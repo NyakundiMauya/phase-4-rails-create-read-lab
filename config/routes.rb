222459b7c86f88cbeadc01e7be92657b62b14638
NyakundiMauya <n.mauya.nm@gmail.com>
2023-07-05 08:43:46 +0300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Index Route
+  get '/plants', to: 'plants#index'
+
+  # Show Route
+  get '/plants/:id', to: 'plants#show'
+
+  # Create Route
+  post '/plants', to: 'plants#create'
+
+
 end
